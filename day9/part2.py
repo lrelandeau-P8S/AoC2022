2006e6d4d640be61a9eddf985a8def30821c43f2
Louis Relandeau <l.relandeau@pick8ship.com>
2022-12-10 14:45:31 +0100

    Day9

--- day9/part2.py
+++ day9/part2.py
@@ -21,14 +21,11 @@ def update():
     dist = np.linalg.norm(vect)
     
     if (abs(vect[0]) == 2 and vect[1]==0) or (abs(vect[1]) == 2 and vect[0]==0):
-        options = [copy.copy(rope[i]), copy.copy(rope[i]), copy.copy(rope[i]), copy.copy(rope[i])]
-        options[0][0] += 1
-        options[1][0] -= 1
-        options[2][1] += 1
-        options[3][1] -= 1
+        options = [[copy.copy(rope[i][0])+1,copy.copy(rope[i][1])] , [copy.copy(rope[i][0])-1,copy.copy(rope[i][1])] , [copy.copy(rope[i][0]),copy.copy(rope[i][1])+1] , [copy.copy(rope[i][0]),copy.copy(rope[i][1])-1]]
         dists = [np.linalg.norm(np.array(rope[i+1])-np.array(option)) for option in options]
         ind = dists.index(min(dists))
         rope[i+1] = copy.copy(options[ind])
+
     elif dist>2.23:
         rope[i+1][0] += vect[0]//abs(vect[0])
         rope[i+1][1] += vect[1]//abs(vect[1])
@@ -39,46 +36,23 @@ for l, move in enumerate(data):
             if move[0] == 'R':
                 if i == 0:
                     rope[i][1] += 1
-                update()
-
             elif move[0] == 'L':
                 if i == 0:
                     rope[i][1] -= 1
-                update()
-
             elif move[0] == 'U':
                 if i == 0:
                     rope[i][0] -= 1
-                update()
-
             elif move[0] == 'D':
                 if i == 0:
                     rope[i][0] += 1
-                update()
+            update()
     
             if i==0:
                 positions[i].append(copy.copy(rope[i]))
             positions[i+1].append(copy.copy(rope[i+1]))
     
-        # if l == 1:
-        #     plt.figure()
-        #     ropeplot = np.zeros(shape=(N,N))
-        #     for r in rope:
-        #         ropeplot[r[0],r[1]] = 255
-        #     plt.imshow(ropeplot, interpolation='nearest', cmap='Greys')
-
-plt.figure()
-headplot = np.zeros(shape=(N,N))
-for pos in positions[0]:
-    headplot[pos[0],pos[1]] = 255
-plt.imshow(headplot, interpolation='nearest', cmap='Greys')
-
-plt.figure()
 tailplot = np.zeros(shape=(N,N))
 for pos in positions[-1]:
-    tailplot[pos[0],pos[1]] = 255
-plt.imshow(tailplot, interpolation='nearest', cmap='Greys')
+    tailplot[pos[0],pos[1]] = 1
 
 print(np.count_nonzero(tailplot))
-
-# plt.show()
